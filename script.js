180bc428fab7bfd8ec4665ffc75655d202034849
mcstellarweb <pandagamer7553@outlook.com>
2024-11-01 11:06:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function toggleModal() {
+    const modal = document.getElementById('configModal');
+    modal.classList.toggle('active');
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+
+function toggleDesktopMode() {
+    // Implementa tu lógica aquí
+}
+
+function toggleAnimations() {
+    // Implementa tu lógica aquí
+}
 
