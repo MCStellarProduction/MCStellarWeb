67e0ef6d1eedcad9ace87594d7bcb69b33f77bc2
mcstellarweb <pandagamer7553@outlook.com>
2024-11-04 19:29:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-function toggleModal() {
-    const modal = document.getElementById('configModal');
-    modal.classList.toggle('active');
-}
-
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-}
-
-function toggleDesktopMode() {
-    // Implementa tu lógica aquí
-}
-
-function toggleAnimations() {
-    // Implementa tu lógica aquí
-}
 
